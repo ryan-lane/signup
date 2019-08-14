a5e8564f3ac6e60a9da75e71501a3bd67777733f
Ryan Lane <rlane@lyft.com>
2019-08-13 23:26:46 -0400

    Add logging config

--- signup/__init__.py
+++ signup/__init__.py
@@ -1,8 +1,26 @@
+from logging.config import dictConfig
+
 from flask import Flask
 from flask_sslify import SSLify
 
 from signup import settings
 
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'formatter': 'default',
+        'stream': 'ext://sys.stdout',
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 static_folder = settings.get('STATIC_FOLDER')
 app = Flask(__name__, static_folder=static_folder)
